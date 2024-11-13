2f6f51e0ac91986bbd66ed8caa8afeae9348f0af
Nash Zangio <nash.zangio@qonto.com>
2024-11-13 18:38:08 +0100

    timer completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,35 +7,46 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-    
+
+
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+
+    var secondsRemaining = 60
+
+    var timer = Timer()
+
+
+
 
     @IBAction func keyPressed(_ sender: UIButton) {
 
-        let eggTimes = [
-            "Soft": 5*60,
-            "Medium": 7*60,
-            "Hard": 12*60
-        ]
+        timer.invalidate()
 
-        let hardness = sender.currentTitle!
-        let result = eggTimes[hardness]!
+        let hardness = sender.currentTitle!  //Soft, Medium, Hard
+            secondsRemaining = eggTimes[hardness]! // 300, 420, 720
 
-        switch hardness{
-        case "Soft":
-            print(result)
-        case "Medium":
-            print(result)
-        case "Hard":
-            print(result)
-        default:
-            print("Error")
-        }
+        //Countdown of seconds to cook
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+
+
+    }
+
+    @objc func updateCounter() {
+        //functionality
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds to cook.")
+            secondsRemaining -= 1
+        } 
     }
 
 
-
-
-
 }
+
+
+
+
+
+
