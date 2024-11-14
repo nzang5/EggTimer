119da0e7239c5e8cc5a2cd4fe28c32900aad3577
Nash Zangio <nash.zangio@qonto.com>
2024-11-14 09:32:33 +0100

    progressBar completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -12,9 +12,10 @@ import Foundation
 class ViewController: UIViewController {
 
 
-    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    var secondsRemaining = 60
+    let eggTimes = ["Soft": 30, "Medium": 42, "Hard": 72]
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
 
 
     @IBOutlet weak var textLabel: UILabel!
@@ -23,23 +24,25 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
 
-        progressBar.progress = 1.0
-
         timer.invalidate()
         let hardness = sender.currentTitle!  //Soft, Medium, Hard
-            secondsRemaining = eggTimes[hardness]! // 300, 420, 720
+        totalTime = eggTimes[hardness]! // 30, 42, 72
+        progressBar.progress = 0.0  //reset progress bar to zero
+        secondsPassed = 0            //reset to zero
+        textLabel.text = hardness    //display which one we picked
 
         //Countdown of seconds to cook
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
 
-
     }
 
     @objc func updateCounter() {
+
         //functionality
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds to cook.")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            print("\(secondsPassed) seconds to cook.")
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
         } else{
             timer.invalidate()
             textLabel.text = "DONE!"
